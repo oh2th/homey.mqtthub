17cf62a9298f7a5df8036ac79bfb7e8e7f850d43
RonnyWinkler <ronny.winkler@web.de>
2024-12-14 13:30:39 +0100

    4.3.0
    Added new device classes and capabilities. Added device setting for cumulative

--- drivers/homie/device.js
+++ drivers/homie/device.js
@@ -11,6 +11,45 @@ class MQTTHomieDiscoveryDevice extends Homey.Device {
         // The Homie Discovery driver adds a pre-configured MQTT Device
         // see driver.onMapDeviceClass(device)
     }
+
+    async onSettings({oldSettings, newSettings, changedKeys}) {
+
+        // Energy settings
+        if (changedKeys.indexOf('set_energy_cumulative') > -1){
+            if (newSettings['set_energy_cumulative']){
+                this.log("onSettings(): set_energy_cumulative SET");
+                await this._setEnergyCumulative(true);
+            }
+            else{
+                this.log("onSettings(): set_energy_cumulative UNSET");
+                await this._setEnergyCumulative(false);
+            } 
+        }
+        if (changedKeys.indexOf('set_energy_home_battery') > -1){
+            if (newSettings['set_energy_home_battery']){
+                this.log("onSettings(): set_energy_home_battery SET");
+                await this._setEnergyHomeBattery(true);
+            }
+            else{
+                this.log("onSettings(): set_energy_home_battery UNSET");
+                await this._setEnergyHomeBattery(false);
+            } 
+        }
+    }
+
+    // Energy settings ================================================================================================
+    async _setEnergyCumulative(value = false){
+        await this.setEnergy(
+            { "cumulative": value }
+        );
+    }
+
+    async _setEnergyHomeBattery(value = false){
+        await this.setEnergy(
+            { "homeBattery": value }
+        );
+    }
+
 }
 
 module.exports = MQTTHomieDiscoveryDevice;
